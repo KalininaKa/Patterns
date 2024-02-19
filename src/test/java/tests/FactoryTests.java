f0e83d9d2de5397a6bc6c5e21f95ba7a361d9cb7
Kalinina Karina <kalinina.karina10@yandex.ru.ru>
2024-02-19 19:32:17 +0300

    add FactoryTests

---
+++ src/test/java/tests/FactoryTests.java
@@ -0,0 +1,56 @@
+package tests;
+
+import factories.DriverTestFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static factories.DriverTestFactory.driver;
+
+public class FactoryTests {
+
+    //тест с использованием разных браузеров
+    @Test
+    public void safariTest() {
+        driver = DriverTestFactory.getTestDriver("safari");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.wikipedia.org/");
+        String title = driver.getTitle();
+        Assertions.assertEquals("Wikipedia", title);
+
+    }
+    @Test
+    public void chromeTest() {
+        driver = DriverTestFactory.getTestDriver("chrome");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.wikipedia.org/");
+        String title = driver.getTitle();
+        Assertions.assertEquals("Wikipedia", title);
+    }
+
+    @Test
+    public void iexploreTest() {
+        driver = DriverTestFactory.getTestDriver("iexplore");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.wikipedia.org/");
+        String title = driver.getTitle();
+        Assertions.assertEquals("Wikipedia", title);
+    }
+
+    @Test
+    public void firefoxTest() {
+        driver = DriverTestFactory.getTestDriver("firefox");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.wikipedia.org/");
+        String title = driver.getTitle();
+        Assertions.assertEquals("Wikipedia", title);
+    }
+
+    @AfterEach
+    public void close() {
+        driver.quit();
+    }
+
+}
